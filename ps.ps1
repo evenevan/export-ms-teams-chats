103ae1db30c27bdaaa8fa0e2d962cb698717834e
Attituding <84680760+Attituding@users.noreply.github.com>
2023-07-23 22:07:36 -0700

    feat: introduce irm/iex ps1 script

---
+++ ps.ps1
@@ -0,0 +1,25 @@
+# heavily based on https://github.com/massgravel/mas-docs/blob/main/get.ps1
+
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$downloadURL = 'https://github.com/attituding/export-ms-teams-chats/archive/refs/heads/main.zip'
+
+$extractedPath = "$([System.IO.Path]::GetTempPath())export-ms-teams-chats"
+$zipPath = "$extractedPath.zip"
+
+Invoke-WebRequest -Uri $downloadURL -OutFile $zipPath
+Expand-Archive -LiteralPath $zipPath -DestinationPath $extractedPath -Force
+
+$innerFolderName = Get-ChildItem -LiteralPath $extractedPath -Name
+$out = "$(Get-Location)/out"
+
+Write-Host "Your chats will be exported to $out"
+
+pwsh.exe -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out
+
+Remove-Item -LiteralPath $zipPath -Recurse
+Remove-Item -LiteralPath $extractedPath -Recurse
