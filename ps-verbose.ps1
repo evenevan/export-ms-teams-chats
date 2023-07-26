6d2257a3decc8aaf887721ea59f1d609b4a153e2
Attituding <84680760+Attituding@users.noreply.github.com>
2023-07-25 20:55:25 -0700

    feat: verbose irm iex

--- ps-verbose.ps1
+++ ps-verbose.ps1
@@ -18,13 +18,13 @@ $innerFolderName = Get-ChildItem -LiteralPath $extractedPath -Name
 $out = "$(Get-Location)/out"
 
 if ($IsMacOS -or $IsLinux) {
-    pwsh -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out
+    pwsh -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out -Verbose
 
 } else {
     try {
-        pwsh.exe -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out
+        pwsh.exe -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out -Verbose
     } catch [System.Management.Automation.CommandNotFoundException] {
-        powershell.exe -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out
+        powershell.exe -File "$extractedPath/$innerFolderName/Get-MicrosoftTeamsChat.ps1" -exportFolder $out -Verbose
     }
 }
 
